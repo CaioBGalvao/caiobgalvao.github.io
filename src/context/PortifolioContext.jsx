2f0003d04163d6cc77ec0780b10fec20b48ef9f4
Caio Galvão <caio_bgalvao@hotmail.com>
2022-07-31 01:14:19 -0300

    Instalação do Prop-types, e o context para o estado global da aplicação

---
+++ src/context/PortifolioContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const PortifolioContext = createContext();
+
+export default PortifolioContext;
---
+++ src/context/ProviderContext.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import PortifolioContext from './PortifolioContext';
+
+function Provider({ children }) {
+  const [data, setData] = useState([]);
+
+  const contextValue = {
+    data,
+    setData,
+  };
+
+  return (
+    <PortifolioContext.Provider value={contextValue}>
+      {children}
+    </PortifolioContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
