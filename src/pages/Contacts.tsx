f551b25aa1e4077727c08d3dda7201746be5f960
Caio Galvão <caio_bgalvao@hotmail.com>
2022-12-26 21:19:17 -0300

    Feat: Área de contato
    Essa área envia e-mail.

---
+++ src/pages/Contacts.tsx
@@ -0,0 +1,110 @@
+import React, {useState} from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import emailjs from '@emailjs/browser';
+import './Contacts.css';
+
+function Contacts() {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
+	const [showAlert, setShowAlert] = useState(false);
+	const [showSuccess, setShowSuccess] = useState(false);
+
+	const {
+    REACT_APP_SERVICE_ID,
+    REACT_APP_TEMPLATE_ID,
+    REACT_APP_PUBLIC_KEY} = process.env;
+
+	const templateParams = {
+		fromName: name,
+		fromEmail: email,
+		toName: 'Caio Galvão',
+		message,
+	};
+
+	const sendEmail = async () => {
+		if (name === '' || email === '' || message === '') {
+			setShowAlert(true);
+		} else {
+			await emailjs
+				.send(
+          REACT_APP_SERVICE_ID || '',
+          REACT_APP_TEMPLATE_ID || '',
+          templateParams,
+          REACT_APP_PUBLIC_KEY || '');
+			setShowAlert(false);
+			setShowSuccess(true);
+			setTimeout(() => {
+				setShowSuccess(false);
+			}, 5000);
+		}
+	};
+
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		await sendEmail();
+	};
+
+	return (
+		<Container fluid className='container-contacts bg-light'>
+			<h1>&lt; Contatos &gt;</h1>
+			{showAlert && (
+				<Alert variant='danger'>Por favor, preencha todos os campos.</Alert>
+			)}
+			<Form onSubmit={handleSubmit}>
+				<Form.Group className='mb-3' controlId='formName'>
+					<Form.Label>Seu nome:</Form.Label>
+					<Form.Control
+						type='input'
+						placeholder='Caio Galvão'
+						value={name}
+						onChange={event => {
+							setName(event.target.value);
+						}}
+					/>
+				</Form.Group>
+
+				<Form.Group className='mb-3' controlId='formBasicEmail'>
+					<Form.Label>Endereço de E-mail</Form.Label>
+					<Form.Control
+						type='email'
+						placeholder='caio@galvao.com'
+						value={email}
+						onChange={event => {
+							setEmail(event.target.value);
+						}}
+					/>
+					<Form.Text className='text-muted'>
+            Não irei compartilhar esse e-mail com ninguem.
+					</Form.Text>
+				</Form.Group>
+
+				<Form.Group className='mb-3' controlId='formMenssage'>
+					<Form.Label>Deixe sua mensagem aqui:</Form.Label>
+					<Form.Control
+						as='textarea'
+						rows={5}
+						value={message}
+						onChange={event => {
+							setMessage(event.target.value);
+						}}
+					/>
+				</Form.Group>
+
+				<Button className='mb-3' variant='primary' type='submit'>
+          Enviar
+				</Button>
+
+				{showSuccess && (
+					<Alert variant='success'>E-mail enviado.</Alert>
+				)}
+			</Form>
+		</Container >
+	);
+}
+
+export default Contacts;
+
