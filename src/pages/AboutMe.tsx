9103aa9bbe30d526a8bec2b9e9a9bf9b371fbf50
Caio Galvão <caio_bgalvao@hotmail.com>
2022-12-29 01:25:51 -0300

    Feat: Texto inserido e formatado
    Adicionado o texto de forma responsiva.

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import './AboutMe.css';
+import SobreMin from '../components/SobreMim';
 
 function AboutMe() {
-	return (
-		<Container fluid className='container-about bg-light'>
-			<h1>&lt; About Me &gt;</h1>
-			<p>Um texto para ocupar aqui</p>
-		</Container >
-	);
+  return (
+    <Container fluid className='container-about bg-light'>
+      <h1 className='mt-5'>&lt; Sobre Mim &gt;</h1>
+      <SobreMin />
+      <Link to='/Contact' className='mb-5'>
+        <Button variant='dark'>Me envie um E-mail</Button>
+      </Link>
+    </Container >
+  );
 }
 
 export default AboutMe;
