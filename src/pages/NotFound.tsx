48c8bfe4352cdf2af9b9047822156438b38f99e8
Caio Galvão <caio_bgalvao@hotmail.com>
2023-01-19 23:22:33 -0300

    Fix: Correção temporária

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,8 @@
 import './NotFound.css';
+import '../components/RootLayout.css';
 
 import { Link, useRouteError } from 'react-router-dom';
-
+import MainNavigation from '../components/MainNavigation';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import { Helmet } from 'react-helmet';
@@ -18,16 +19,19 @@ function ErrorPage() {
         <title>Não Encontrado - Portifólio</title>
         <meta name='description' content='Não há nada aqui.' />
       </Helmet>
-      <Container fluid id="error-page" className='container-nfound bg-light'>
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
-        <Link to='/'>
-          <Button variant='dark'>Vamos para Home!</Button>
-        </Link>
-      </Container>
+      <MainNavigation />
+      <main className='main-size'>
+        <Container fluid id="error-page" className='container-nfound bg-light'>
+          <h1>Oops!</h1>
+          <p>Desculpa, um erro ocorreu.</p>
+          <p>
+            <i>{error.statusText || error.message}</i>
+          </p>
+          <Link to='/'>
+            <Button variant='dark'>Vamos para Home!</Button>
+          </Link>
+        </Container>
+      </main>
     </>
   );
 }
